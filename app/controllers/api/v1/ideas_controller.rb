2b6ee6437a112c614ec0141d3c8afbada4a70009
nyoken <dk525p.ss16@gmail.com>
2020-11-16 01:14:06 +0900

    GET時のレスポンス内容を仕様に合わせて変更

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -6,14 +6,15 @@ module Api
       def index
         if params[:category_name]
           if category = Category.find_by(name: params[:category_name])
-            ideas = category.ideas
-            render status: 200, json: { status: :ok, message: "Selected Category Ideas", data: ideas }
+            serialize_ideas(category.ideas)
+            render status: :ok, json: { data: @ideas }
           else
-            render status: 404, json: { status: :not_found, message: "Category Not Found" }
+            render status: :not_found
           end
         else
           ideas = Idea.all.includes(:category)
-          render status: 200, json: { status: :ok, message: "All Ideas", data: ideas }
+          serialize_ideas(ideas)
+          render status: :ok, json: { data: @ideas }
         end
       end
 
@@ -25,15 +26,23 @@ module Api
         end
 
         if idea.save
-          render status: 201, json: { status: :created, data: idea }
+          render status: :created, json: { data: idea }
         else
-          render status: 422, json: { status: :unprocessable_entity, data: idea.errors }
+          render status: :unprocessable_entity, json: { data: idea.errors }
         end
       end
 
       private
         def idea_params
           params.permit(:body, :category_id)
+        end
+
+        def serialize_ideas(ideas)
+          @ideas = []
+          ideas.each do |idea|
+            hash = { id: idea.id, category: idea.category.name, body: idea.body }
+            @ideas = @ideas.push(hash)
+          end
         end
     end
   end

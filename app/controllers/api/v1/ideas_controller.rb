fb02ecde7ed1a1b7cc8649e24cba91b77bb79088
nyoken <dk525p.ss16@gmail.com>
2020-11-15 21:46:37 +0900

    アイデア取得機能の作成

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,6 +3,20 @@
 module Api
   module V1
     class IdeasController < ApplicationController
+      def index
+        if params[:category_name]
+          if category = Category.find_by(name: params[:category_name])
+            ideas = category.ideas
+            render status: 200, json: { status: :ok, message: "Selected Category Ideas", data: ideas }
+          else
+            render status: 404, json: { status: :not_found, message: "Category Not Found" }
+          end
+        else
+          ideas = Idea.all.includes(:category)
+          render status: 200, json: { status: :ok, message: "All Ideas", data: ideas }
+        end
+      end
+
       def create
         idea = Idea.new(idea_params)
         if !(params[:category_name].blank? || params[:body].blank?)
--- spec/requests/api/v1/ideas_request_spec.rb
+++ spec/requests/api/v1/ideas_request_spec.rb
@@ -3,6 +3,46 @@
 require "rails_helper"
 
 RSpec.describe "IdeasAPI", type: :request do
+  describe "Index" do
+    before do
+      # リクエストするCategoryのideaを生成
+      @category = create(:category)
+      @ideas = create_list(:idea, 3, category: @category)
+      # リクエストするCategoryではないideaを生成
+      other_category = create(:category)
+      @other_idea = create(:idea, category: other_category)
+    end
+
+    context "params:[category_name:]がある場合" do
+      it 'リクエストされたcategoryに対応するideaを取得する' do
+        get '/api/v1/ideas', params: { category_name: @category.name }
+        # @ideasは含まれるが、@other_ideaは含まれない
+        expect(response.body).to include(@ideas.first.body)
+        expect(response.body).not_to include(@other_idea.body)
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json['data'].length).to eq(3)
+      end
+
+      it 'リクエストされたcategoryが存在しない場合' do
+        get '/api/v1/ideas', params: { category_name: "not_exist_category" }
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context "params:[category_name:]がない場合" do
+      it '全てのideaを取得する' do
+        get '/api/v1/ideas'
+        # @ideasも@other_ideaも含まれる
+        expect(response.body).to include(@ideas.first.body)
+        expect(response.body).to include(@other_idea.body)
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json['data'].length).to eq(4)
+      end
+    end
+  end
+
   describe "Create" do
     context "paramsが存在する場合" do
       it "既存のCategoryに新しいIdeaを追加する" do
